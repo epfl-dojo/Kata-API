5ed7d4dff365774998bfd927dc1189e102fe8577
saphirevert <jerome.cosandey@gmail.com>
2020-12-10 16:19:28 +0100

    [feature] All endpoints finished - PUT /beer/:id - PATCH /beer/:id GET /breweries GET /breweries/id GET /categories GET /categories/:id GET /style GET /style/:id

--- src/app/app/Models/Beer.php
+++ src/app/app/Models/Beer.php
@@ -10,5 +10,5 @@ class Beer extends Model
     use HasFactory;
     // https://stackoverflow.com/a/28277980
     public $timestamps = false;
-    protected $fillable = ['name', 'descript', 'last_mod'];
+    protected $fillable = ['name', 'descript', 'last_mod', 'cat_id', 'style_id', 'abv', 'ibu', 'srm', 'upc', 'filepath', 'add_user', 'last_mod'];
 }
--- src/app/routes/api.php
+++ src/app/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Beer;
+use App\Models\Breweries;
+use App\Models\Categories;
+use App\Models\Styles;
 use \Illuminate\Support\Facades\Validator;
 /*
 |--------------------------------------------------------------------------
@@ -36,9 +39,13 @@ Route::post('/beer', function(Request $request) {
 
 
     $validator = Validator::make($request->json()->all(), [
-        'name' => ['required', 'integer'],
+        'name' => ['required'],
         'descript' => 'required',
     ]);
+    // $validator = Validator::make($request->json()->all(), [
+    //     'name' => ['required', 'integer'],
+    //     'descript' => 'required',
+    // ]);
 
 
     $date = new DateTime();
@@ -51,7 +58,7 @@ Route::post('/beer', function(Request $request) {
           Log::error('fail');
           return 400;
       } else {
-          Log::error('success');
+          Log::info('success');
           Beer::create(['name' => $request->input('name'), 'brewery_id' => $request->input('brewery_id'), 'cat_id' => $request->input('cat_id'), 'style_id' => $request->input('style_id'), 'abv' => $request->input('abv'), 'ibu' => $request->input('ibu'), 'srm' => $request->input('srm'), 'upc' => $request->input('upc'), 'filepath' => $request->input('filepath'), 'descript' => $request->input('descript'), 'add_user' => $request->input('add_user'), 'last_mod' => $formatedDate]);
           return 204;
       }
@@ -72,9 +79,94 @@ Route::delete('/beer/{id}', function ($id) {
     Beer::destroy($id);
 });
 
-// Route::put('/beer/{id}', function ($id) {
-//     return Beer::find($id);
-// });
-// Route::patch('/beer/{id}', function ($id) {
-//     return Beer::find($id);
-// });
+Route::put('/beer/{id}', function (Request $request, $id) {
+    // $tmpBeer = Beer::has('comments', '>=', 3)->get();
+
+    $validator = Validator::make($request->json()->all(), [
+        'name' => ['required'],
+        'brewery_id' => ['required', 'integer'],
+        'cat_id' => ['required', 'integer'],
+        'style_id' => ['required', 'integer'],
+        'abv' => ['required', 'float'],
+        'ibu' => ['required', 'float'],
+        'srm' => ['required', 'float'],
+        'upc' => ['required', 'integer'],
+        'filepath' => 'required',
+        'descript' => 'required',
+        'add_user' => ['required', 'integer'],
+        'last_mod' => ['required', 'integer'],
+    ]);
+
+    if($validator->fails()){
+        Log::error('fail');
+        return 400;
+    } else {
+        Log::info('success');
+        $Beer = Beer::find($id);
+        $Beer->name = $request->input('name');
+        $Beer->save();
+        return 204;
+    }
+
+    // Beer::where('id', $id)
+    //       ->update(['name' => 'toto'])
+});
+Route::patch('/beer/{id}', function (Request $request, $id) {
+    // $tmpBeer = Beer::has('comments', '>=', 3)->get();
+    $date = new DateTime();
+    $formatedDate = $date->format('Y-m-d H:i:s');
+    $data = $request->json()->all();
+    $data['last_mod'] = $formatedDate;
+    $validator = Validator::make($data, [
+        'name' => ['string'],
+        'brewery_id' => ['integer'],
+        'cat_id' => ['integer'],
+        'style_id' => ['integer'],
+        'abv' => ['float'],
+        'ibu' => ['float'],
+        'srm' => ['float'],
+        'upc' => ['integer'],
+        'filepath' => ['string'],
+        'descript' => ['string'],
+        'add_user' => ['integer'],
+    ]);
+
+    if($validator->fails()){
+        Log::error('fail');
+        return 400;
+    } else {
+        Log::info('success');
+        $beer = Beer::find($id);
+        $beer->fill($data);
+        $beer->save();
+        return 204;
+    }
+
+});
+
+
+Route::get('/breweries', function () {
+    return Breweries::all();
+});
+
+Route::get('/breweries/{id}', function ($id) {
+    return Breweries::find($id);
+});
+
+
+Route::get('/categories', function () {
+    return Categories::all();
+});
+
+Route::get('/categories/{id}', function ($id) {
+    return Categories::find($id);
+});
+
+
+Route::get('/styles', function () {
+    return Styles::all();
+});
+
+Route::get('/styles/{id}', function ($id) {
+    return Styles::find($id);
+});
